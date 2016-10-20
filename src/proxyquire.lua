92b1f0c2fe50027b1464de9a30b03ba4f2b2e822
Ryan Hartlage <ryanplusplus@gmail.com>
2016-10-20 05:48:05 -0400

    Fixed lit package

--- src/proxyquire.lua
+++ src/proxyquire.lua
@@ -1,3 +1,13 @@
+--[[lit-meta
+  name = 'ryanplusplus/proxyquire'
+  version = '1.0.2'
+  description = 'Allows Lua modules to be overridden during testing. See https://github.com/thlorenz/proxyquire.'
+  tags = { 'testing' }
+  license = 'MIT'
+  author = { name = 'Ryan Hartlage' }
+  homepage = 'https://github.com/ryanplusplus/proxyquire.lua'
+]]
+
 return function(module, proxied_dependencies)
   local cache_copy = {}
 
